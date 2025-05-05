4276c94cbdb79fe15d73c64cdfcb336974b0ee32
Himanshu_Raturi <166353708+Himanshu0508Raturi@users.noreply.github.com>
2025-05-05 23:37:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,3 @@ async def predict(file: UploadFile = File(...)):
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
